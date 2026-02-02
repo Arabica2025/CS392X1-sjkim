af9aa3810192fc1c34ff10be70fdfea2d01edef6
Sungjun Kim <sjkim23@bu.edu>
2026-02-02 13:07:20 -0500

    hw2 finished Assign02_01.java and Assign02_03.java

--- assigns/02/Code/Assign02_02.java
+++ assigns/02/Code/Assign02_02.java
@@ -28,9 +28,51 @@ public class Assign02_02 {
     public static <T extends Comparable<T> > int indexOf(T[] a, T key) {
 	// Please give a recursive implementation of 'indexOf' that is
 	// equivalent to the above one
-    }
+        if (key == null || a == null || a.length == 0){
+            return -1;
+        }
+        int lower = 0;
+        int upper = a.length - 1;
+        if (lower > upper){
+            return -1;
+        }
+        /* 
+        if (upper % 2 == 1){
+            upper += 1;
+        }
+        */
+        int mid = lower + (upper - lower) / 2;
+        int sign = key.compareTo(a[mid]);
+        // if we found the key
+        if (sign == 0) {
+            return mid;
+        } 
 
-    public void main(String[] argv) {
+        if (sign < 0){ // key < a[mid]
+
+            T[] left = Arrays.copyOfRange(a, lower, mid);
+            return indexOf(left, key);
+        } else {//if (sign > 0){ // key > a[mid]
+
+            T[] right = Arrays.copyOfRange(a,mid, upper);
+            return mid + 1 + indexOf(right, key);
+        } 
+        /*else {
+            return -1;
+        }
+            */
+        
+    }
+    
+
+    public static void main(String[] args) {
 	// Please write some testing code for your implementation of 'indexOf'
+        Integer[] testArr = {1,2,3,4,5};
+        System.out.println(testArr.length); // expect 11
+        System.out.println("test indexOf 3: " + indexOf(testArr, 3));
+        System.out.println("test indexOf 2: " + indexOf(testArr, 2));
+        System.out.println("test indexOf 5: " + indexOf(testArr, 5));
+        System.out.println("test indexOf 10: " + indexOf(testArr, 10));
+        System.out.println("test indexOf 11: " + indexOf(testArr, 11));
     }
 }
