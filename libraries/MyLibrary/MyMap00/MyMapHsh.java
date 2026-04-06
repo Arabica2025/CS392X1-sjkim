0f1520b423b2f02bf65e0cae910fc9290592c1ae
Sungjun Kim <sjkim23@bu.edu>
2026-04-06 03:34:46 -0400

    hw8 done

---
+++ libraries/MyLibrary/MyMap00/MyMapHsh.java
@@ -0,0 +1,11 @@
+package MyLibrary.MyMap00;
+
+import MyLibrary.LnList.*;
+import MyLibrary.LnStrm.*;
+import MyLibrary.FnTuple.*;
+
+
+public class MyMapHsh<K, V> {
+    private LnList<FnTupl2<K, V>> table[];
+
+}
